93e0b858c1c45547986510d571399ef3e33fbdb8
Mathis <93223257+ML-Design-web@users.noreply.github.com>
2022-03-16 20:10:26 +0100

    UPDATE

--- include/sidenav.php
+++ include/sidenav.php
@@ -1,6 +1,17 @@
 <div class="side-nav">
-    <div class="sn-title">
-        
+    <div class="d-flex sn-title">
+        <span>MENU</span>
+    </div>
 
+    <div class="d-flex sn-items">
+        <ul>
+            <li>accueil</li>
+            <li>S'identifier</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
 </div>
--- index.php
+++ index.php
@@ -27,6 +27,12 @@
         <?php
         include_once('include/sidenav.php')
         ?>
+
+        <div class="scale-container">
+            <div class="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam a iusto sequi ipsum iste alias eaque non commodi maxime fuga expedita obcaecati minima at soluta cum id laudantium, ratione optio incidunt esse perferendis mollitia. Beatae, totam? Necessitatibus esse quas quidem sapiente consectetur nihil odio porro quam suscipit praesentium? Vitae repudiandae corrupti doloremque quos, mollitia molestiae amet, dolorem itaque quae ducimus consectetur ex magni. Obcaecati perferendis possimus iste inventore non aperiam dolore sed repellat modi, minus dolorum quod placeat officia earum magni nesciunt accusantium consequatur vitae praesentium? Odio eum voluptate aspernatur suscipit, dicta inventore, esse iste iure odit cumque illum cum unde. Quas ea corporis tempora maiores, similique repudiandae nihil quis nam doloribus explicabo beatae quod minus reprehenderit odio odit deserunt dolore numquam aut! Suscipit, rem et mollitia sed voluptates aliquam laboriosam facilis blanditiis quisquam amet unde fuga veritatis delectus, id enim vitae eos possimus. Nemo animi facilis accusantium reiciendis sapiente inventore sed quisquam minus libero. Suscipit aliquid nemo saepe tempora rerum. Repellendus ipsam in praesentium odio corporis provident inventore ipsa animi deleniti laborum. Maiores molestias tempora animi cumque quis! Magnam vel illo rem blanditiis ad maiores qui non saepe dignissimos illum, accusamus, maxime veritatis? Aut, suscipit! Similique et minima eos?
+            </div>
+        </div>
     </main>
 
 
